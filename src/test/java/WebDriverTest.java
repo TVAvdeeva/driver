4d3ea617d0e8444ac0b321150bd301ed21597462
Алекса <74459397+TVAvdeeva@users.noreply.github.com>
2023-04-25 22:02:16 +0200

    Remove files

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -10,14 +10,19 @@ import java.time.Duration;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import java.util.*;
 
 import java.util.concurrent.TimeUnit;
+
+import static org.apache.commons.lang3.StringUtils.substring;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebDriverTest {
     protected  WebDriver driver;
     private final String login=System.getProperty("login");
     private final String password = System.getProperty("password");
+     private String  url=System.getProperty("url");
+
     private static Logger logger = LogManager.getLogger(WebDriverTest.class);
     private ChromeOptions options = new ChromeOptions();
 
@@ -27,9 +32,10 @@ public class WebDriverTest {
     }
 
 
-    private void authUser() {
+    private void authUser() throws InterruptedException {
         driver.findElement(By.cssSelector(".header3__button-sign-in")).click();
         WebElement form = driver.findElement(By.xpath("//form[@action = '/login/']"));
+        Thread.sleep(100);
         form.findElement(By.xpath(".//input[@name='email']")).sendKeys(login);
         form.findElement(By.xpath(".//input[@name='password']")).sendKeys(password);
         form.findElement(By.xpath(".//button[@type='submit']")).click();
@@ -39,6 +45,8 @@ public class WebDriverTest {
     @BeforeAll
     public static void beforeTest() {
         WebDriverManager.chromedriver().setup();
+
+
     }
 
     @BeforeEach
@@ -52,15 +60,15 @@ public class WebDriverTest {
         options.addArguments("--headless");
         options.addArguments("--start-maximized");
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
-        driver.get("https://duckduckgo.com/");
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+        driver.get("https://duckduckgo.com");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(150));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search_form_input_homepage")));
         clearAndEnter(By.id("search_form_input_homepage"), "ОТУС");
         driver.findElement(By.id("search_button_homepage")).submit();
-        String element = driver.findElement(By.xpath("//article[@id='r1-0']/div[2]/h2/a/span")).getText();
         Thread.sleep(1000);
-        Assertions.assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение современным ...", element);
+        String element = driver.findElement(By.xpath("//article[@id='r1-0']/div[2]/h2/a/span")).getText();
+        assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение современным ...", element);
 
     }
 
@@ -69,21 +77,26 @@ public class WebDriverTest {
     //     driver = new ChromeDriver(options);
     //     driver.get("https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818");
     //  driver.findElement(By.xpath("//span[@class='image-block']/a")).click();
-    //    WebElement element = driver.findElement(By.xpath("//div[@class='pp_pic_holder light_rounded']"));
+    //    WebElement element = driver.findElement(By.cssSelector("div.pp_pic_holder.light_rounded"));
     //    Assertions.assertTrue(element.isDisplayed());
 
     //  }
 
     @Test
     public void otusTest() throws InterruptedException {
+
+         if (url.trim().endsWith("/")) {
+            url= substring(url,0,url.length()-1);
+        }
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
-        driver.get("https://otus.ru/");
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+        driver.get(url);
         authUser();
-        String actual = driver.getTitle();
-        Assertions.assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям", actual);
-        String  cookiesGet = String.valueOf(driver.manage().getCookies());
         Thread.sleep(1000);
+        String actual = driver.getTitle();
+        assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям", actual);
+        String  cookiesGet = String.valueOf(driver.manage().getCookies());
+
         logger.info(cookiesGet);
 
     }
